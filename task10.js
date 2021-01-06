ed33f3aac1da72aea02b70704a4a7e423034587a
Nkosi97 <nkosinathisiyeni97@gmail.com>
2021-01-06 12:18:46 +0200

    edited task10

--- task10.js
+++ task10.js
@@ -1,13 +1,9 @@
-function vowels(str) {
-    let vowelArr = ["a", "e", "i", "o", "u"];
-    let strVowel = "";
-    str = str.toLowerCase();
-    vowelArr.forEach((vowel) => {
-      if (str.includes(vowel)) {
-        strVowel += vowel;
-      }
-    });
-    return strVowel;
+function getVowels(str) {
+  var vowels = [];
+  if (str.match(/[aeiou]/gi)) {
+    vowels.push(str.match(/[aeiou]/gi));
   }
+  return vowels.toString().replace(/,/g, "");
+}
 
- 
+
