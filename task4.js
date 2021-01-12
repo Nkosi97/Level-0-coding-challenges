468611c3f558039e57bccb83edc16ba83f357ec3
Nkosi97 <nkosinathisiyeni97@gmail.com>
2021-01-12 10:55:28 +0200

    changes done

--- task4.js
+++ task4.js
@@ -1,10 +1,9 @@
 function sum(num1, num2) {
   let sum = num1 + num2;
-  sum.toString();
-  if (num1 === 3 || num2 === 3 && sum.includes("3")){
-    return true;
-  } else{
-    return false;
+  
+  if (num1 === 3 || num2 === 3 || sum.toString().match(/3/g)){
+  return true;
+  } else {
+  return false;
   }
-}
-
+  }
